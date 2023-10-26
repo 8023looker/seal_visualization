307fe0e5288fdf92917ae62ee139853f06a25b0e
Keer.Lu <996401827@qq.com>
2023-10-26 16:48:35 +0800

    onresize when maintaining ratios of 16:9

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,39 +1,42 @@
 <template>
     <router-view />
-    <div class="app backgrounded" v-if="render_main">
-        <div class="header">
-            <Title></Title>
-            <SwitchView></SwitchView>
-        </div>
-        <div class="left-panel">
-           
-        </div>
-        <div class="bottom-panel">
-            <div class="time-axis">
-                <TimeAxis></TimeAxis>
+    <div class="app backgrounded">
+        <div class="app-canvas" v-if="render_main"
+            :style="{ left: (window_width - render_width) / 2 + 'px', top: (window_height - render_height) / 2 + 'px', width: render_width + 'px', height: render_height + 'px' }">
+            <div class="header">
+                <Title></Title>
+                <SwitchView></SwitchView>
             </div>
-        </div>
-        <div class="main-panel">
-            <div id="main-view" v-if="render_main">
-                <!-- <Graph
-                    v-show="cur_view === 'graph' || overlay_view === 'graph'"
-                    :canvas_width="canvas_width"
-                    :canvas_height="canvas_height"
-                ></Graph> -->
-                <Timeline
-                    v-show="
-                        cur_view == 'timeline' || overlay_view === 'timeline'
-                    "
-                    :canvas_width="canvas_width"
-                    :canvas_height="canvas_height"
-                ></Timeline>
-                <!-- <GeoMap
-                    v-show="showGeoMap"
-                    :canvas_width="canvas_width"
-                    :canvas_height="canvas_height"
-                ></GeoMap> -->
+            <div class="left-panel">
+            
             </div>
-        </div>
+            <div class="bottom-panel">
+                <div class="time-axis">
+                    <TimeAxis></TimeAxis>
+                </div>
+            </div>
+            <div class="main-panel">
+                <div id="main-view" v-if="render_main">
+                    <!-- <Graph
+                        v-show="cur_view === 'graph' || overlay_view === 'graph'"
+                        :canvas_width="canvas_width"
+                        :canvas_height="canvas_height"
+                    ></Graph> -->
+                    <Timeline
+                        v-show="
+                            cur_view === 'timeline' || overlay_view === 'timeline'
+                        "
+                        :canvas_width="canvas_width"
+                        :canvas_height="canvas_height"
+                    ></Timeline>
+                    <!-- <GeoMap
+                        v-show="showGeoMap"
+                        :canvas_width="canvas_width"
+                        :canvas_height="canvas_height"
+                    ></GeoMap> -->
+                </div>
+            </div>
+        </div> 
     </div>
 </template>
 
@@ -68,6 +71,12 @@ export default {
             renderComponent: true,
             canvas_width: null,
             canvas_height: null,
+
+            window_width: null, // 当前窗口实际大小
+            window_height: null,
+
+            render_width: null, // 需要渲染的窗口大小
+            render_height: null,
         };
     },
     components: {
@@ -107,14 +116,17 @@ export default {
         setCanvas() { // 确定画布的大小
             let width = window.innerWidth;
             let height = window.innerHeight;
+
+            this.window_width = window.innerWidth;
+            this.window_height = window.innerHeight;
             // 将画布默认为16:9
             if (width / height > 16 / 9) { // 以height为基准
                 width = height * 16 / 9
             } else { // 以width为基准
                 height = width * 9 / 16
             }
-            this.canvas_width = width;
-            this.canvas_height = height;
+            this.render_width = width;
+            this.render_height = height;
         },
         initialize() {
             const self = this
@@ -137,11 +149,12 @@ export default {
         window.addEventListener("resize", function() {
             // 在窗口大小调整时执行的操作
             that.renderComponent = false;
+            that.render_main = false;
             that.initialize();
             setTimeout(() => {
                 that.renderComponent = true;
             }, 500);
-            console.log("窗口大小已调整");
+            console.log("窗口大小已调整", that.render_width, that.window_width);
         });
         // window.onresize = () => {
         //     this.renderComponent = false;
@@ -221,82 +234,73 @@ body {
 .app {
     height: 100vh;
     width: 100vw;
-
-    $title-height: 6.4vh;
-    $left-panel-width: 15vw;
-    $bottom-panel-height: 10vh;
-
-    $switch-lang-width: $left-panel-width;
-
-    .header {
+    .app-canvas {
         position: absolute;
-        height: $title-height;
-        width: 100vw;
-        // background-color: rgba(240, 248, 255, 0.085);
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-    }
+        
+        $title-height: 6.4%;
+        $left-panel-width: 15%;
+        $bottom-panel-height: 10%;
 
-    .left-panel {
-        position: absolute;
-        width: $left-panel-width;
-        // height: 100vh - $title-height - $bottom-panel-height;
-        height: 100vh - $title-height;
-        top: $title-height;
-        // font-family: FZQINGKBYSJF;
-        background-color: rgba(13, 79, 137, 0.05);
+        $switch-lang-width: $left-panel-width;
 
-        // $book-list-height: 60%;
-        $book-list-height: 40%;
-    }
-
-    .bottom-panel {
-        position: absolute;
-        height: $bottom-panel-height;
-        left: $left-panel-width;
-        width: calc(100vw - $left-panel-width);
-        bottom: 0vh;
-        background-color: rgba(127, 255, 212, 0.119);
-        display: flex;
-        flex-direction: row;
-
-        .time-axis {
-            position: relative;
-            // width: 100vw - $switch-lang-width;
+        .header {
+            position: absolute;
+            height: $title-height;
             width: 100%;
-            height: 100%;
-            // background-color: rgba(176, 196, 222, 0.212);
+            // background-color: rgba(240, 248, 255, 0.085);
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
         }
-    }
 
-    .main-panel {
-        position: absolute;
-        height: 100vh - $title-height - $bottom-panel-height;
-        width: calc(100vw - $left-panel-width);
-        left: $left-panel-width;
-        top: $title-height;
-        // background-color: rgba(240, 255, 255, 0.282);
-
-        #main-view {
-            & > div {
-                position: absolute;
-            }
-            position: relative;
-            height: 100%;
-            width: 100%;
-            background-color: rgba(240, 255, 255, 0.6);
+        .left-panel {
+            position: absolute;
+            width: $left-panel-width;
+            // height: 100vh - $title-height - $bottom-panel-height;
+            height: 100% - $title-height;
+            top: $title-height;
             // font-family: FZQINGKBYSJF;
+            background-color: rgba(13, 79, 137, 0.05);
         }
-    }
 
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity 1s; /* 过渡效果的持续时间 */
-    }
-    .fade-enter-from,
-    .fade-leave-to {
-        opacity: 0; /* 初始透明度 */
+        .bottom-panel {
+            position: absolute;
+            height: $bottom-panel-height;
+            left: $left-panel-width;
+            width: calc(100% - $left-panel-width);
+            bottom: 0%;
+            background-color: rgba(127, 255, 212, 0.119);
+            display: flex;
+            flex-direction: row;
+
+            .time-axis {
+                position: relative;
+                // width: 100vw - $switch-lang-width;
+                width: 100%;
+                height: 100%;
+                // background-color: rgba(176, 196, 222, 0.212);
+            }
+        }
+
+        .main-panel {
+            position: absolute;
+            height: 100% - $title-height - $bottom-panel-height;
+            width: calc(100% - $left-panel-width);
+            left: $left-panel-width;
+            top: $title-height;
+            // background-color: rgba(240, 255, 255, 0.282);
+
+            #main-view {
+                & > div {
+                    position: absolute;
+                }
+                position: relative;
+                height: 100%;
+                width: 100%;
+                background-color: rgba(240, 255, 255, 0.6);
+                // font-family: FZQINGKBYSJF;
+            }
+        }
     }
 }
 </style>
